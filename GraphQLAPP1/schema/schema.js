3be9eac16bc649e7d49cc11f3e4c8b6ccba83953
Adrian Pearman <adrianpearman12@gmail.com>
2017-11-27 19:50:35 -0500

    complettion of first app and moved to own folder. initial commit for app2.

--- GraphQLAPP1/schema/schema.js
+++ GraphQLAPP1/schema/schema.js
@@ -7,7 +7,8 @@ const{
   GraphQLString,
   GraphQLInt ,
   GraphQLSchema,
-  GraphQLList
+  GraphQLList,
+  GraphQLNonNull
 } = graphql
 
 // const users = [
@@ -72,6 +73,49 @@ const RootQuery = new GraphQLObjectType({
   }
 })
 
+// allows for information in the database to be changed
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: {type: new GraphQLNonNull(GraphQLString)}, //the same as required
+        age: {type: new GraphQLNonNull(GraphQLInt)},
+        companyId: {type: GraphQLString}
+      },
+      resolve(parentValue, {firstName, age}){
+        return axios.post(`http://localhost:3000/users`, { firstName, age})
+        .then(response => response.data)
+      }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: {type: new GraphQLNonNull(GraphQLString)},
+        firstName: {type: GraphQLString},
+        age: {type: GraphQLInt},
+        companyId: {type: GraphQLString}
+      },
+      resolve(parentValue, args){
+        return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+        .then(response => response.data)
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: {type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve(parentValue, args){
+        return axios.delete(`http://localhost:3000/users/${args.id}`)
+        .then(response => response.data)
+      }
+    }
+  }
+})
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: mutation
 })
